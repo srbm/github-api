25288c953f334e8d01fa6a665f563be9d7df9075
srbm <shawnrbm@gmail.com>
2019-07-10 14:49:52 -0700

    error handling

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ function getRepos(userName) {
             // console.log(responseJson);
             displayResults(responseJson);
         })
-        .catch();
+        .catch(err => {
+            if (err.message === 'responseJson[0] is undefined') {
+                $('#results-list, #js-error-message').empty();
+                $('#results h2').hide();
+                $('#js-error-message').text(`Github could not find that username`);
+            } else {
+                $('#results-list, #js-error-message').empty();
+                $('#results h2').hide();
+                $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            }
+            
+          });
 }
 
 function displayResults(responseJson) {
-    $('#results-list').empty();
+    $('#results-list, #js-error-message').empty();
     console.log(responseJson);
-    $('#results h2').append(`${responseJson[0].owner.login}`);
+    $('#results h2').append(`${responseJson[0].owner.login}`).show();
     responseJson.forEach(repo => {
         
         $('#results-list').append(
